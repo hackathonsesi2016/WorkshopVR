38b9514d8fa0e13079999cbc41596bfe006cde11
kaznowski <kkdasilva@gmail.com>
2016-10-11 20:23:40 -0300

    Adiciona Imagem de referencia do nosso controle bluetooth. Adiciona Script que eu utilizei pra reconhecer qual era o mapeamento do controle, para caso alguém queira ver o que eu fiz. Adiciona ao GazeInputModule um campo para ser o ClickButton, já que iremos utilizar o controle como método de Input, assim os participantes poderão usar o controle para disparar um evento de Click na interface da Unity. Adiciona a partir da nova função do GazeInputModule um script de exemplo "SendClickEventExample" que altera a cor de um cubo aleatóriamente a partir de um click no botão que está na tela

---
+++ Assets/HackathonSESI/Code/KeyCodeCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KeyCodeCheck : MonoBehaviour
+{
+    private Text _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<Text>();
+        _text.text = "";
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            Debug.Log("Detected key code: " + e.keyCode);
+            _text.text = "Detected key code: " + e.keyCode;
+        }
+
+    }
+}
---
+++ Assets/HackathonSESI/Code/SendClickEventExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SendClickEventExample : MonoBehaviour
+{
+    public Renderer Cube;
+
+    public void DebugTest(string s)
+    {
+        Debug.Log(s);
+        Cube.material.color = Random.ColorHSV();
+    }
+
+}
