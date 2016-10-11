ee843cee46ee334e51762cdd7b553bcc3ebc463e
kaznowski <kkdasilva@gmail.com>
2016-10-11 17:49:03 -0300

    Commitando o projeto inicial já contendo parte do demonstrativo de VR, com um controle customizado e a movimentação pelo analógico do controle Bluetooth.
    - Ainda falta mapear os botões físicos.

---
+++ Assets/HackathonSESI/Code/ActiveEventAfterLook.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ActiveEventAfterLook : MonoBehaviour
+{
+    [SerializeField]
+    protected float _activeDelay = 2;
+
+    private bool _looking = false;
+    private bool _canFire = true;
+    private float _currentTime = 0;
+    private EventTrigger _eventTrigger;
+
+    protected void Awake()
+    {
+        _eventTrigger = GetComponent<EventTrigger>();
+        if (!_eventTrigger)
+        {
+            _eventTrigger = gameObject.AddComponent<EventTrigger>();
+
+            EventTrigger.Entry onPointerEnter = new EventTrigger.Entry();
+            onPointerEnter.eventID = EventTriggerType.PointerEnter;
+            onPointerEnter.callback.AddListener(OnLookObject);
+            _eventTrigger.triggers.Add(onPointerEnter);
+
+            EventTrigger.Entry onPointerExit = new EventTrigger.Entry();
+            onPointerExit.eventID = EventTriggerType.PointerExit;
+            onPointerExit.callback.AddListener(OnUnLookObject);
+            _eventTrigger.triggers.Add(onPointerExit);
+        }
+    }
+
+    public void Update()
+    {
+        if (!_looking)
+            return;
+        _currentTime += Time.deltaTime;
+        if (_currentTime >= _activeDelay)
+        {
+            InternalTriggerLook();
+        }
+    }
+
+    private void InternalTriggerLook()
+    {
+        if (!_canFire)
+            return;
+        _canFire = false;
+        _currentTime = 0;
+        OnTriggerLook();
+    }
+
+    public virtual void OnTriggerLook()
+    {
+        Debug.Log("Active Object with Look. " + gameObject.name);
+    }
+
+    public void OnLookObject(BaseEventData e)
+    {
+        //Debug.Log("Look object: " + gameObject.name);
+        _looking = true;
+    }
+
+    public void OnUnLookObject(BaseEventData e)
+    {
+        //Debug.Log("UnLook object: " + gameObject.name);
+        _looking = false;
+        _canFire = true;
+        _currentTime = 0;
+    }
+
+
+}
---
+++ Assets/HackathonSESI/Code/CanvasFollower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasFollower : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform _target;
+
+    private Vector3 _offSet;
+
+
+    protected void Awake()
+    {
+        if (_target == null)
+            return;
+        _offSet = this.transform.position - _target.position;
+    }
+
+    protected void LateUpdate()
+    {
+        if (_target == null)
+            return;
+        this.transform.position = _target.position + _offSet;
+    }
+
+}
